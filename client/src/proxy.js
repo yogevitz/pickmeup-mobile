d1b97d4cf547ee81ce77bfd61a86c6d71839af3a
Yogev Shlomovitz <yogevshlomovitz@gmail.com>
2020-04-18 16:28:56 +0300

    feat: updating proxy from backoffice proxy

--- client/src/proxy.js
+++ client/src/proxy.js
@@ -21,33 +21,33 @@ export async function getShuttle(shuttleID) {
 }
 
 export async function createSupervisor(body) {
-  const { data } = await client.post(`/api/createSupervisor`, body);
+  const { data } = await client.post(`/createSupervisor`, body);
   return data;
 }
 
 export async function setSupervisor(body) {
-  const { data } = await client.post(`/api/setSupervisor`, body);
+  const { data } = await client.post(`/setSupervisor`, body);
   return data;
 }
 
 export async function createShuttle(body) {
-  const { data } = await client.post(`/api/createShuttle`, body);
+  const { data } = await client.post(`/createShuttle`, body);
   return data;
 }
 
 export async function setShuttle(body) {
-  const { data } = await client.post(`/api/setShuttle`, body);
+  const { data } = await client.post(`/setShuttle`, body);
   return data;
 }
 
 
 export async function createRider(body) {
-  const { data } = await client.post(`/api/createRider`, body);
+  const { data } = await client.post(`/createRider`, body);
   return data;
 }
 
 export async function setRider(body) {
-  const { data } = await client.post(`/api/setRider`, body);
+  const { data } = await client.post(`/setRider`, body);
   return data;
 }
 
@@ -56,8 +56,28 @@ export async function getAllShuttleRiders(shuttleID) {
   return data;
 }
 
-export async function markRider(body) {
-  const { data } = await client.post(`/api/markRider`, body);
+export async function getShuttleRidersByShuttle(shuttleID) {
+  const { data } = await client.get(`/getShuttleRidersByShuttle/${shuttleID}`);
   return data;
 }
 
+export async function getShuttleRidersByRider(riderID) {
+  const { data } = await client.get(`/getShuttleRidersByRider/${riderID}`);
+  return data;
+}
+
+export async function getLiftRiders(params) {
+  const { shuttleID, date, direction } = params;
+  const { data } = await client.get(`/getLiftRiders/${shuttleID}/${date}/${direction}`);
+  return data;
+}
+
+export async function setLiftRiderMark(body) {
+  const { data } = await client.post(`/setLiftRiderMark`, body);
+  return data;
+}
+
+export async function setLiftRiderApproved(body) {
+  const { data } = await client.post(`/setLiftRiderApproved`, body);
+  return data;
+}
