fc5eec5002811c4f0d8a17dbdab74d5333eb99b0
Yogev Shlomovitz <yogevshlomovitz@gmail.com>
2020-04-18 16:56:47 +0300

    feat: use new api functions for lift riders

--- client/src/pages/report/report.js
+++ client/src/pages/report/report.js
@@ -3,12 +3,14 @@ import Table from "../../components/Table";
 import CardHeader from "@material-ui/core/CardHeader";
 import CardContent from "@material-ui/core/CardContent";
 import Button from "@material-ui/core/Button";
+import Grid from '@material-ui/core/Grid';
 import Card from "@material-ui/core/Card";
 import Autocomplete from '@material-ui/lab/Autocomplete';
 import TextField from '@material-ui/core/TextField';
+import AddIcon from '@material-ui/icons/Add';
 import QRScanner from '../../QRScanner';
 
-import { getAllShuttleRiders, markRider, getAllRiders } from '../../proxy';
+import { getLiftRiders, setLiftRiderMark, getAllRiders } from '../../proxy';
 
 const columns = [
   { title: 'Name', field: 'riderName' },
@@ -35,21 +37,21 @@ class Report extends React.Component {
     if (selectedIDs.length > this.checked.length) {
       newChecked = selectedIDs.filter(x => this.checked.indexOf(x) < 0);
       this.checked.push(...newChecked);
-      newChecked.forEach(async checked => await markRider({
+      newChecked.forEach(async checked => await setLiftRiderMark({
         shuttleID: '1',
         riderID: checked,
-        date: '31/03/2020',
-        direction: 'Morning',
+        date: '18-04-2020',
+        direction: 'Afternoon',
         mark: '1',
       }));
     } else {
       newUnChecked = this.checked.filter(x => selectedIDs.indexOf(x) < 0);
       this.checked = this.checked.filter(_ => newUnChecked.indexOf(_) < 0);
-      newUnChecked.forEach(async unChecked => await markRider({
+      newUnChecked.forEach(async unChecked => await setLiftRiderMark({
         shuttleID: '1',
         riderID: unChecked,
-        date: '31/03/2020',
-        direction: 'Morning',
+        date: '18-04-2020',
+        direction: 'Afternoon',
         mark: '0',
       }));
     }
@@ -63,9 +65,9 @@ class Report extends React.Component {
 
   async componentDidMount() {
     const allRiders = await getAllRiders();
-    let shuttleRiders = await getAllShuttleRiders(1);
-    shuttleRiders = shuttleRiders.map(this.getRiderRowData);
-    this.setState({ shuttleRiders: shuttleRiders, riders: allRiders });
+    let liftRiders = await getLiftRiders({ shuttleID: '1', date: '18-04-2020', direction: 'Afternoon' });
+    liftRiders = liftRiders.map(this.getRiderRowData);
+    this.setState({ shuttleRiders: liftRiders, riders: allRiders });
   }
 
   handleScan = id => {
@@ -82,7 +84,6 @@ class Report extends React.Component {
 
   handleAdd = async () => {
     this.setState({ isAddRiderShown: !this.state.isAddRiderShown });
-    console.log('Add Rider');
   };
 
   onChooseNewRider = (event, newValue) => {
@@ -93,54 +94,57 @@ class Report extends React.Component {
   render() {
     const { shuttleRiders, query, showScanner, isAddRiderShown, riders } = this.state;
     return (
-      <div>
-        <Card>
-          <CardHeader title={'Beer Sheva Morning Report'} />
-          {
-            showScanner
+      <Card>
+        <CardHeader title={'Shoval Bus 1 Afternoon Report'} />
+        <CardContent>
+          {showScanner
             ? (
-                <div>
-                  <QRScanner onScan={this.handleScan} />
-                  <Button style={{ marginTop: '10px' }} variant="contained" onClick={this.onClickHideScanner}>
-                    Hide Scanner
-                  </Button>
-                </div>)
-            :
-              <Button variant="contained" onClick={this.onClickShowScanner}>
+              <div>
+                <QRScanner onScan={this.handleScan} />
+                <Button style={{ marginTop: '10px', marginBottom: '10px' }} variant="contained" onClick={this.onClickHideScanner}>
+                  Hide Scanner
+                </Button>
+              </div>
+            )
+            : (
+              <Button style={{ marginBottom: '10px' }} variant="contained" onClick={this.onClickShowScanner}>
                 Scan
               </Button>
+            )
           }
-          <CardContent>
-            {
-              isAddRiderShown &&
-              (
-                <div>
-                  <Autocomplete
-                    id="rider-select"
-                    options={riders.map(_ => `${_.name} (${_.sid})`)}
-                    getOptionLabel={(option) => option}
-                    onChange={this.onChooseNewRider}
-                    renderInput={(params) =>
-                      <TextField {...params} label="Rider" variant="outlined" />}
-                  />
-                  <Button style={{ marginTop: '10px', marginBottom: '20px' }} variant="contained" onClick={this.addRider}>
-                    Add Rider
-                  </Button>
-                </div>
-              )
-            }
-            <Table
-              title=""
-              columns={columns}
-              selection={true}
-              data={shuttleRiders}
-              query={query}
-              onSelectionChange={this.onSelectionChange}
-              handleAdd={this.handleAdd}
-            />
-          </CardContent>
-        </Card>
-      </div>
+          {isAddRiderShown && (
+              <div>
+                <Grid container justify="center" spacing={1}>
+                  <Grid item xs={8}>
+                    <Autocomplete
+                      id="rider-select"
+                      options={riders.map(_ => `${_.name} (${_.riderID})`)}
+                      getOptionLabel={(option) => option}
+                      onChange={this.onChooseNewRider}
+                      renderInput={(params) =>
+                        <TextField {...params} label="Rider" variant="outlined" />}
+                    />
+                  </Grid>
+                  <Grid item>
+                    <Button style={{ marginTop: '10px', marginBottom: '20px' }} variant="contained" onClick={this.addRider}>
+                      <AddIcon />
+                    </Button>
+                  </Grid>
+                </Grid>
+              </div>
+            )
+          }
+          <Table
+            title=""
+            columns={columns}
+            selection={true}
+            data={shuttleRiders}
+            query={query}
+            onSelectionChange={this.onSelectionChange}
+            handleAdd={this.handleAdd}
+          />
+        </CardContent>
+      </Card>
     );
   }
 }
